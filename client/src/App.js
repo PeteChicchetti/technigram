27dbe0600851ec27a1b65e538e600f0c2c61902a
PeteChicchetti <pj0524@gmail.com>
2022-12-12 15:19:28 -0500

    removed pages and import/routes in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,14 +8,7 @@ function App() {
       <>
         <Navbar />
         <Routes>
-          <Route 
-            path='/' 
-            element={<SearchBooks />} 
-          />
-          <Route 
-            path='/saved' 
-            element={<SavedBooks />} 
-          />
+          
           <Route 
             path='*'
             element={<h1 className='display-2'>Wrong page!</h1>}
