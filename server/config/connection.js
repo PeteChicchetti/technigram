46c74d2f6ef54325c3ae5f21050c52b38f92d883
PeteChicchetti <pj0524@gmail.com>
2022-12-12 15:37:29 -0500

    removed file structure

--- server/config/connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/techmatchup', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-module.exports = mongoose.connection;
