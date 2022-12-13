60228249a5fc8d097b6bbd0aec157688617f05d7
BenCottrell <bencottrell48@yahoo.com>
2022-12-13 16:27:21 -0500

    centered items in footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,8 +4,9 @@ import { AiFillGithub } from "react-icons/ai";
 
 const Footer = () => {
     return (
+        <div className="footer-contain">
         <div className="footer container-fluid">
-            <Row className='container-fluid' style={{ margin: '0px', justifyContent: 'center', alignItems: 'center' }}>
+            <Row className='container-fluid'>
                 <Col md='4' className="footer-text">
                     <h3>Made by: <a href="https://github.com/ThatBallinGuy">Tristan Ballin</a>, <a href="https://github.com/PeteChicchetti">Pete Chicchetti</a>, <a href="https://github.com/BenjaminCottrell">Ben Cottrell</a>, and <a href="https://github.com/StyngerBee">Curran Duke</a>.</h3>
                 </Col>
@@ -13,11 +14,10 @@ const Footer = () => {
                     <h3>Licensed under <a href="https://opensource.org/licenses/mit-license.php">MIT</a></h3>
                 </Col>
                 <Col md='4' className="footer-text">
-                    <Col>
-                    <a className='github' href="https://github.com/PeteChicchetti/technigram" target="_blank" rel="noopener noreferrer"><AiFillGithub /></a>
-                    </Col>
+                    <h3>GitHub Repo:  <a className='github' href="https://github.com/PeteChicchetti/technigram" target="_blank" rel="noopener noreferrer"><AiFillGithub /></a></h3>
                 </Col>
             </Row>
+        </div>
         </div>
     )
 }
