eed46be4b4b0705d279ee7c06a8c21160c19eaf4
BenCottrell <bencottrell48@yahoo.com>
2022-12-13 15:15:02 -0500

    added github links to names in footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -4,14 +4,17 @@ import { Row, Col } from "react-bootstrap";
 const Footer = () => {
     return (
         <div className="footer container-fluid">
-        <Row className='container-fluid' style={{ margin:'0px', justifyContent: 'center', alignItems: 'center'}}>
-            <Col md='6' className='footer-text'>
-                <h3>Developed by Pete Chicchetti, Tristan Ballin, Ben Cottrell, and Curran Duke</h3>
-            </Col>
-            <Col md='6' className='footer-text'>
-                <h3>Licensed by MIT</h3>
-            </Col>
-        </Row>
+            <Row className='container-fluid' style={{ margin: '0px', justifyContent: 'center', alignItems: 'center' }}>
+                <Col md='4' className="footer-text">
+                    <h3><a href="https://github.com/ThatBallinGuy">Tristan Ballin</a>, <a href="https://github.com/PeteChicchetti">Pete Chicchetti</a>, <a href="https://github.com/BenjaminCottrell">Ben Cottrell</a>, and <a href="https://github.com/StyngerBee">Curran Duke</a>.</h3>
+                </Col>
+                <Col md='4' className="footer-text">
+                    <h3>Licensed by MIT</h3>
+                </Col>
+                <Col md='4' className="footer-text">
+                    <h3>Licensed by MIT</h3>
+                </Col>
+            </Row>
         </div>
     )
 }
