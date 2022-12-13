75d02cbda3d837205f2cb183327a3f8ae4fd95fa
BallinGuy <tballin2000@hotmail.com>
2022-12-13 13:49:09 -0500

    added created at in models and added client side queries

--- server/models/Reaction.js
+++ server/models/Reaction.js
@@ -5,6 +5,11 @@ const reactionSchema = new Schema({
     type: String,
     required: true,
   },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (timestamp) => dateFormat(timestamp),
+  },
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User'
