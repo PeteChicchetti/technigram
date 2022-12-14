6ff6d051f14b1ea57e6e8a7b787ae303d86ccd95
PeteChicchetti <pj0524@gmail.com>
2022-12-14 14:15:43 -0500

    added basic layout for cards. create post and post card

--- client/src/components/Builds/index.jsx
+++ client/src/components/Builds/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Container, Row, Col, Card } from "react-bootstrap";
 import { Link } from 'react-router-dom';
+import { GrAdd } from "react-icons/gr";
 
 
 const Build = () => {
   return (
-    <main>
-         <Link to="/createpost">
-          <button>Create a Post</button>
-        </Link>
-    </main>
+    <Container id='buildContainer'>
+      <Row id='buildRow' className='container-fluid'>
+        <Col id='buildCol'>
+            <Card id='addPost'>
+            <Link id='postText' to="/createpost"><GrAdd /></Link>
+            </Card>
+            <Col>
+            <Card id='buildPost'>
+              <h1>What is Lorem Ipsum?</h1>
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            </Card>
+            </Col>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
