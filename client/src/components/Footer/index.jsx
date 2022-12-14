c4522f587b684fe21f29892bfc2c7a2fdd1eb33e
BenCottrell <bencottrell48@yahoo.com>
2022-12-14 11:50:08 -0500

    removed margin from row

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
         <div className="footer-contain">
         <div className="footer container-fluid">
-            <Row className='container-fluid'>
+            <Row className='container-fluid' style={{ margin: "0px" }}>
                 <Col md='4' className="footer-text">
                     <h3>Made by: <a href="https://github.com/ThatBallinGuy">Tristan Ballin</a>, <a href="https://github.com/PeteChicchetti">Pete Chicchetti</a>, <a href="https://github.com/BenjaminCottrell">Ben Cottrell</a>, and <a href="https://github.com/StyngerBee">Curran Duke</a>.</h3>
                 </Col>
