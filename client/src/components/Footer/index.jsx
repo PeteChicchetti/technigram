72640c2271a4da7872352d86727f1d11002608fd
BenCottrell <bencottrell48@yahoo.com>
2022-12-16 16:57:43 -0500

    added text align to dev credits

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className="footer container-fluid">
             <Row className='footer-row container-fluid' style={{ margin: "0px" }}>
                 <Col md='4' className="footer-text">
-                    <h3>Made by: <a href="https://github.com/ThatBallinGuy">Tristan Ballin</a>, <a href="https://github.com/PeteChicchetti">Pete Chicchetti</a>, <a href="https://github.com/BenjaminCottrell">Ben Cottrell</a>, and <a href="https://github.com/StyngerBee">Curran Duke</a>.</h3>
+                    <h3 className="developers">Made by: <a href="https://github.com/ThatBallinGuy">Tristan Ballin</a>, <a href="https://github.com/PeteChicchetti">Pete Chicchetti</a>, <a href="https://github.com/BenjaminCottrell">Ben Cottrell</a>, and <a href="https://github.com/StyngerBee">Curran Duke</a>.</h3>
                 </Col>
                 <Col md='4' className="footer-text">
                     <Row id="license-credit" className="container-fluid"><div>Licensed under <a className= "license" href="https://opensource.org/licenses/mit-license.php">MIT</a></div>
