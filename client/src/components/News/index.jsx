818398f00cd0e007058556beb6a2571194c14919
Curran Duke <cdsonics@yahoo.com>
2022-12-16 14:01:45 -0500

    fixed structure of container

--- client/src/components/News/index.jsx
+++ client/src/components/News/index.jsx
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Row, Col, Card } from "react-bootstrap";
 
 
@@ -6,55 +6,52 @@ const News = () => {
   const url = `https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=6798423f8ca44d86a70f8c879be30589`;
   const [results, setResults] = useState([])
 
-const getNews = () => {
-  fetch(url)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(data) {
-      const results = data
-      console.log(results);
-      setResults(results.articles);
-      
-    })
-    .catch(function(error) {
-      console.error("There was an error parsing the response as JSON: " + error);
-    });
-}
+  const getNews = () => {
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        const results = data
+        console.log(results);
+        setResults(results.articles);
+
+      })
+      .catch(function (error) {
+        console.error("There was an error parsing the response as JSON: " + error);
+      });
+  }
 
 
-useEffect(() => {
-  // var req = new Request(url);
-  getNews();
-}, [])
+  useEffect(() => {
+    getNews();
+  }, [])
 
-return (
-  <>
-  {results.map((result, index) => {
-    return (
-      <Container id='newsContainer' key={index} >
-    <Row id='newsRow' className='container-fluid' >
-      <Col id='newsCol'>
-          <Col>
-          <Card id='newsPost' >
-            <div id='newsHeader'>
-              <a id='newsTitle' href={result.url} target='_blank'>{result.title}</a> 
-            </div>
-            <img src={result.urlToImage} className='newsImage'></img>
-            <p id='newsContent'>{result.description}</p>
-            <span id='newsInfo'>Author: {result.author}, Source: {result.source.name}</span>
-          </Card>
+  return (
+    <>
+      <Container id='newsContainer'  >
+        <Row id='newsRow' className='container-fluid' >
+          <Col id='newsCol'>
+            <Col>
+              {results.map((result, index) => {
+                return (
 
+                  <Card id='newsPost' key={index}>
+                    <div id='newsHeader'>
+                      <a id='newsTitle' href={result.url} target='_blank'>{result.title}</a>
+                    </div>
+                    <img src={result.urlToImage} className='newsImage'></img>
+                    <p id='newsContent'>{result.description}</p>
+                    <span id='newsInfo'>Author: {result.author}, Source: {result.source.name}</span>
+                  </Card>
+                )
+              })}
+            </Col>
           </Col>
-      </Col>
-    </Row>
-  </Container>
-
-    )
-  
-  })}
- </>
-)
+        </Row>
+      </Container>
+    </>
+  )
 }
 
 export default News;
