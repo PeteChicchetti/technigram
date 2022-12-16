924d29a75d9f7408bb5207cbfca78789de67660e
PeteChicchetti <pj0524@gmail.com>
2022-12-16 16:00:07 -0500

    moved submit type from card to the edit card

--- client/src/components/SingleBuild/index.jsx
+++ client/src/components/SingleBuild/index.jsx
@@ -38,7 +38,7 @@ const SingleBuild = () => {
                                 <textarea id='editBuildContent' placeholder='Edit Content'></textarea>
                             <div style={{ display: 'flex', justifyContent: 'space-between'}} >
                                 <span>
-                                    <span id='editIcon' onClick=''><BiSave/></span>
+                                    <span id='editIcon' type='submit' onClick=''><BiSave/></span>
                                     <span id='deleteIcon'><MdDeleteForever /></span>
                                 </span>
                                 <span id='singleBuildInfo'>Posted by: {post.user.username} At: {post.createdAt}</span>
@@ -52,7 +52,7 @@ const SingleBuild = () => {
                         </div>
                             <p id='singleBuildContent'>{post.content}</p>
                         <div style={{ display: 'flex', justifyContent: 'space-between'}} >
-                        <span><span id='editIcon' onClick={() => setEditMode(!editMode)}><CiEdit/></span><span id='deleteIcon' type='submit'><MdDeleteForever /></span></span>
+                        <span><span id='editIcon' onClick={() => setEditMode(!editMode)}><CiEdit/></span><span id='deleteIcon'><MdDeleteForever /></span></span>
                         <span id='singleBuildInfo'>Posted by: {post.user.username} At: {post.createdAt}</span>
                         </div>
                     </Card>
