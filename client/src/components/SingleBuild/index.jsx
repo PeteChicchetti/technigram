004f5969ccee25eb4b973a602b15d7a314243978
BallinGuy <tballin2000@hotmail.com>
2022-12-16 17:23:37 -0500

    added edit and delete functionality

--- client/src/components/SingleBuild/index.jsx
+++ client/src/components/SingleBuild/index.jsx
@@ -6,10 +6,12 @@ import { CiEdit } from "react-icons/ci";
 import { MdDeleteForever } from "react-icons/md";
 import { BiSave } from "react-icons/bi";
 
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 
 import { QUERY_POST } from '../../utils/queries';
+import { UPDATE_POST } from '../../utils/mutations';
+import { DELETE_POST } from '../../utils/mutations';
 
 const SingleBuild = () => {
     const [editMode, setEditMode] = useState(false)
@@ -19,8 +21,37 @@ const SingleBuild = () => {
         variables: { postid: postid },
     });
     const post = data?.post || {};
-    console.log(postid);
-    console.log(data);
+
+    const [title, setTitle] = useState(post.title);
+    const [content, setContent] = useState(post.content);
+
+
+    const [addTitle, { error }] = useMutation(UPDATE_POST);
+    const [addContent, { error2 }] = useMutation(UPDATE_POST);
+    const [deletePost, { error3 }] = useMutation(DELETE_POST);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        const contentdata = await addContent({
+            variables: { postid: postid, content: content },
+        });
+        const titledata = await addTitle({
+            variables: { postid: postid, title: title },
+        });
+        setTitle(post.title);
+        setContent(post.content);
+        setEditMode(!editMode)
+        
+    };
+    const handleDelete = async (event) => {
+        event.preventDefault();
+        const titledata = await deletePost({
+            variables: { postid: post._id },
+        });
+    };
+
+
+
     if (loading) {
         return <div>Loading...</div>;
     }
@@ -29,38 +60,45 @@ const SingleBuild = () => {
             <Row id='singleBuildRow' className='container-fluid'>
                 <Col id='singleBuildCol'>
 
-                    {editMode ? 
-                    <Card id='singleBuildPost' key={post._id}>
-                        <form>
+                    {editMode ?
+                        <Card id='singleBuildPost'>
+                            <form onSubmit={handleFormSubmit}>
+                                <div id='singleBuildHeader'>
+                                    <input
+                                        id='editBuildTitle'
+                                        value={title}
+                                        onChange={(event) => setTitle(event.target.value)}
+                                    />
+                                </div>
+                                <textarea
+                                    id='editBuildContent'
+                                    value={content}
+                                    onChange={(event) => setContent(event.target.value)} />
+                                <div style={{ display: 'flex', justifyContent: 'space-between' }} >
+                                    <span>
+                                        <button type='submit'><span id='editIcon' ><BiSave /></span></button>
+                                        <span id='deleteIcon' onClick={handleDelete}><MdDeleteForever /></span>
+                                    </span>
+                                    <span id='singleBuildInfo'>Posted by: {post.user.username} At: {post.createdAt}</span>
+                                </div>
+                            </form>
+                        </Card>
+                        :
+                        <Card id='singleBuildPost'>
                             <div id='singleBuildHeader'>
-                                <input id='editBuildTitle' placeholder='Edit Title'></input>
+                                <h1 id='singleBuildTitle'>{post.title}</h1>
                             </div>
-                                <textarea id='editBuildContent' placeholder='Edit Content'></textarea>
-                            <div style={{ display: 'flex', justifyContent: 'space-between'}} >
-                                <span>
-                                    <span id='editIcon' type='submit' onClick=''><BiSave/></span>
-                                    <span id='deleteIcon'><MdDeleteForever /></span>
-                                </span>
+                            <p id='singleBuildContent'>{post.content}</p>
+                            <div style={{ display: 'flex', justifyContent: 'space-between' }} >
+                                <span><span id='editIcon' onClick={() => setEditMode(!editMode)}><CiEdit /></span><span id='deleteIcon' onClick={handleDelete}><MdDeleteForever /></span></span>
                                 <span id='singleBuildInfo'>Posted by: {post.user.username} At: {post.createdAt}</span>
                             </div>
-                        </form>
-                    </Card> 
-                    :
-                    <Card id='singleBuildPost' key={post._id}>
-                        <div id='singleBuildHeader'>
-                            <h1 id='singleBuildTitle'>{post.title}</h1>
-                        </div>
-                            <p id='singleBuildContent'>{post.content}</p>
-                        <div style={{ display: 'flex', justifyContent: 'space-between'}} >
-                        <span><span id='editIcon' onClick={() => setEditMode(!editMode)}><CiEdit/></span><span id='deleteIcon'><MdDeleteForever /></span></span>
-                        <span id='singleBuildInfo'>Posted by: {post.user.username} At: {post.createdAt}</span>
-                        </div>
-                    </Card>
+                        </Card>
 
                     }
 
                     {post.reactions.map((reaction) => (
-                        <Card id='singleBuildPost' key={reaction.id}>
+                        <Card id='singleBuildPost' key={reaction._id}>
                             <p id='singleBuildComment'>{reaction.comment}</p>
                             <span id='singleBuildInfo'>Posted by: {reaction.user.username} At: {reaction.createdAt}</span>
                         </Card>
