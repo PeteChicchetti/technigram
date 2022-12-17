6d08b17f4f1da6c9716f5c27c27b795d4253e42c
BallinGuy <tballin2000@hotmail.com>
2022-12-17 15:49:45 -0500

    reaction edit works

--- client/src/components/Comments/index.jsx
+++ client/src/components/Comments/index.jsx
@@ -8,34 +8,26 @@ import { BiSave } from "react-icons/bi";
 
 import { useMutation } from '@apollo/client';
 
-import { QUERY_POST } from '../../utils/queries';
-import { UPDATE_POST } from '../../utils/mutations';
+import { UPDATE_REACTION } from '../../utils/mutations';
 import { DELETE_REACTION } from '../../utils/mutations';
 
 const Comments = ({ reaction, post }) => {
-     const [editMode, setEditMode] = useState(false)
+    const [editMode, setEditMode] = useState(false)
 
     console.log(reaction);
-    // const [title, setTitle] = useState(post.title);
-    // const [content, setContent] = useState(post.content);
+    const [comment, setComment] = useState(reaction.comment);
 
+    const [addComment, { error2 }] = useMutation(UPDATE_REACTION);
+    const [deleteReaction, { error3 }] = useMutation(DELETE_REACTION);
 
-    // const [addTitle, { error }] = useMutation(UPDATE_POST);
-    // const [addContent, { error2 }] = useMutation(UPDATE_POST);
-     const [deleteReaction, { error3 }] = useMutation(DELETE_REACTION);
-
-    // const handlePostSubmit = async (event) => {
-    //     event.preventDefault();
-    //     const contentdata = await addContent({
-    //         variables: { postid: postid, content: content },
-    //     });
-    //     const titledata = await addTitle({
-    //         variables: { postid: postid, title: title },
-    //     });
-    //     setTitle(post.title);
-    //     setContent(post.content);
-    //     setEditMode(!editMode)
-    // };
+    const handleReactionSubmit = async (event) => {
+        event.preventDefault();
+        const commentdata = await addComment({
+            variables: { reactionid: reaction._id, comment: comment },
+        });
+        setComment(reaction.comment);
+        setEditMode(!editMode)
+    };
 
     const handleReactionDelete = async (event) => {
         event.preventDefault();
@@ -47,13 +39,37 @@ const Comments = ({ reaction, post }) => {
 
 
     return (
-        <Card id='singleBuildPost'>
-            <p id='singleBuildComment'>{reaction.comment}</p>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }} >
-                <span><span id='editIcon' onClick={() => setEditMode(!editMode)}><CiEdit /></span><span id='deleteIcon' onClick={handleReactionDelete}><MdDeleteForever /></span></span>
-                <span id='singleBuildInfo'>Posted by: {reaction.user.username} At: {reaction.createdAt}</span>
-            </div>
-        </Card>
+        <>
+        {editMode ?
+            <Card id='singleBuildPost'>
+                <form onSubmit={handleReactionSubmit}>
+                    <input
+                        id='singleBuildComment'
+                        value={comment}
+                        onChange={(event) => setComment(event.target.value)}
+                    />
+                    <div style={{ display: 'flex', justifyContent: 'space-between' }} >
+                        <span>
+                            <button type='submit'><span id='editIcon' ><BiSave /></span></button>
+                            <span id='deleteIcon' onClick={handleReactionDelete}><MdDeleteForever /></span>
+                        </span>
+                        <span id='singleBuildInfo'>Posted by: {reaction.user.username} At: {reaction.createdAt}</span>
+                    </div>
+                </form>
+            </Card>
+            :
+            <Card id='singleBuildPost'>
+                <p id='singleBuildComment'>{reaction.comment}</p>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }} >
+                    <span>
+                        <span id='editIcon' onClick={() => setEditMode(!editMode)}><CiEdit /></span>
+                        <span id='deleteIcon' onClick={handleReactionDelete}><MdDeleteForever /></span>
+                    </span>
+                    <span id='singleBuildInfo'>Posted by: {reaction.user.username} At: {reaction.createdAt}</span>
+                </div>
+            </Card>
+        }
+        </>
     );
 };
 
